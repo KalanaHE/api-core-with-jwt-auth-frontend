8ceb6bff1ef2decbf26c5ee584d898fe36104011
Kalana Hettiarachchi <kalana@orelit.com>
2022-07-18 00:13:19 +0530

    prod build added

---
+++ build/static/js/678.d5a5a31e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_web=self.webpackChunkmy_web||[]).push([[678],{19678:function(e,u,s){s.r(u);var a=s(1128);u.default=a.g}}]);
+//# sourceMappingURL=678.d5a5a31e.chunk.js.map
